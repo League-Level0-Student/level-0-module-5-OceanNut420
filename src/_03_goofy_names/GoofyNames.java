a0c36f5f7aee03b686114c29a812addd04b050cb
OceanNut420 <54760319+OceanNut420@users.noreply.github.com>
2020-03-20 18:24:51 -0700

    Update GoofyNames.java

--- src/_03_goofy_names/GoofyNames.java
+++ src/_03_goofy_names/GoofyNames.java
@@ -17,18 +17,21 @@ public class GoofyNames {
 		System.out.println(name.toUpperCase());
 		// 3. Loop through each character of the name (steps 4 - 6).
 		//    HINT: Use .length() to determine the number of characters in the String.
-
+		for(int i=0; i<name.length(); i++) {
+		
+			
+		
 			// 4. Create a String variable to store the next character of the name
 			//    using .substring(start, end)
 			//    HINT: replace 'start' and 'end' to get String with character at i 
-			
+			name.substring();
 			// 5. Use MODULO to set this variable to upper case for EVEN characters
 			//    and lower case for ODD characters.
 
 			
 			// 6. ADD this String  (containing 1 char) to the goofyName String
 
-		
+	}
 		// 7. Use pop-up to show user their Goofy name
 
 	}
