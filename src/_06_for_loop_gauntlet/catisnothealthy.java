2c0d032980f7d055b99639660188e0a2b39bf816
OceanNut420 <54760319+OceanNut420@users.noreply.github.com>
2020-04-10 18:24:44 -0700

    Update catisnothealthy.java

--- src/_06_for_loop_gauntlet/catisnothealthy.java
+++ src/_06_for_loop_gauntlet/catisnothealthy.java
@@ -16,10 +16,80 @@ public class catisnothealthy {
 //			e = e-1;
 //		}
 		//3.
-		int e = 2;
-		for(int i = 0; i < 50; i++) {
-			System.out.println(e);
-			e = e+2;
+//		int e = 2;
+//	for(int i = 0; i < 50; i++) {
+//		System.out.println(e);
+//			e = e+2;
+//			}
+		//4.
+//		int e = 1;
+//	for(int i = 0; i < 50; i++) {
+//			System.out.println(e);
+//				e = e+2;
+//				}
+		//5.
+//		int e = 1;
+//		for(int i = 0; i < 500; i++) {
+//			System.out.print(e);
+//			if(e % 2 == 0) {
+//				System.out.println(" is even");
+//			}
+//			}
+//			e = e+1;
+//		}
+		//6.
+//		int e = 7;
+//		for(int i = 0; i < 111; i++) {
+//			System.out.println(e);
+//			e = e+7;
+//		}
+		//7.
+//		int e = 2007;
+//		int bruh = 0;
+//		for(int i = 0; i < 14; i++) {
+//			System.out.println("In " + e + ", i was " + bruh + " years old.");
+//			e = e+1;
+//			bruh = bruh + 1;
+//		}
+		//1.
+//		for(int i = 0; i < 3; i++) {
+//			for(int z = 0; z < 3; z++) {
+//				System.out.print(i);
+//				System.out.print(" ");
+//				System.out.println(z);
+//			}
+//		}
+		//2.
+//		int a = 1;
+//		int b = 2;
+//		int c = 3;
+//		for(int i = 0; i < 3; i++) {
+//			for(int z = 0; z < 1; z++) {
+//				System.out.print(a);
+//				System.out.print(b);
+//				System.out.println(c);
+//				a = a + 3;
+//				b = b + 3;
+//				c = c + 3;
+//			}
+//		}
+		//3.
+//		int a = 1;
+//		for(int i = 0; i < 10; i++) {
+//			for(int z = 0; z < 10; z++) {
+//				if(z % 10 == 0 && i > 0) {
+//					System.out.println("");
+//				}
+//
+//				System.out.print(a + " ");
+//			a = a + 1;
+//											}
+//	}	
+		//4.
+		for(int i = 0; i < 6; i++) {
+			for(int z = 0; z < 1; z++) {
+				System.out.println();
+				
 			}
 		}
 		
@@ -27,6 +97,10 @@ public class catisnothealthy {
 		
 		
 		
+		
+		
+		
+		
+		
+		}
 	}
-
-}
