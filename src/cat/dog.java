d90beb1dc7202d9b18993df7ebff0ea1db609a9e
OceanNut420 <54760319+OceanNut420@users.noreply.github.com>
2020-05-29 18:27:10 -0700

    ewewe

--- src/cat/dog.java
+++ src/cat/dog.java
@@ -4,11 +4,15 @@ public class dog {
 
 	public static void main(String[] args) {
 	
-		for(int i = 0; i < 100000; i++) {
-			System.out.println("mom mom mom mom mom mom mom mom mom mom mom mom mom mom mom mom mom mom");
+		for(int i = 0; i < 1000000; i++) {
+			System.out.print("mom cat dog ");
+			if(i % 15 == 0) {
+				System.out.println("");
+				
+			}
+
 		}
-		
-		
+				
 		
 		
 		
